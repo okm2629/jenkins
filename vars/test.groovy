35ff1c6447512b3724db9b526b522bb8e82d5498
Tim <okm262906@gmail.com>
2025-11-15 01:53:09 +0800

    add library and project 1

---
+++ vars/test.groovy
@@ -0,0 +1,11 @@
+def call(String executor){
+    script {
+        if ( executor == "Ithome" ){
+            echo "Hi Ben"
+        } else if (executor == "Jenkins" ){
+            echo "Hi Jenkins"
+        } else {
+            echo "unrecognizable"
+        }
+    }
+}
